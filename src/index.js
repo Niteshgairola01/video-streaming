2ded297375cc3c20d417af6e1d59a51b66b0d269
Niteshgairola01 <niteshgairolapapl@gmail.com>
2025-06-25 15:48:44 +0530

    create utilities for asycn handling, api error and response

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
+import app from './app.js';
 
 dotenv.config({
     path: "./env"
 });
 
-connectDB();
+const port = process.env.PORT || 8000;
+
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log(`Sever is running at port : ${port}`);
+    });
+
+    app.on("error", (err) => {
+        console.log("Error", err);
+    });
+}).catch(err => {
+    console.log("Error connecting DB ! ! !", err);
+
+})
